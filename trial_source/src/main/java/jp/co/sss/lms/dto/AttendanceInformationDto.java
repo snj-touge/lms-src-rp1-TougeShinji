aed56fc99a565f261901643e92d0ce1c74efff5b
snj_toge <snj_toge@IBW-A25-019.ibw02.local>
2026-08-26 17:05:51 +0900

    add:峠 Task.57 AttendanceInformationDtoの作成
    勤怠情報確認画面用DTOを新規作成

---
+++ trial_source/src/main/java/jp/co/sss/lms/dto/AttendanceInformationDto.java
@@ -0,0 +1,25 @@
+package jp.co.sss.lms.dto;
+
+import lombok.Data;
+
+/**
+ * 勤怠情報確認画面用DTO
+ * 
+ * @author 峠伸治 - Task.57
+ */
+@Data
+public class AttendanceInformationDto {
+	
+	/** LMSユーザID */
+	private Integer lmsUserId;
+	/** ユーザID */
+	private Integer userId;
+	/** ユーザ名 */
+	private String userName;
+	/** コース名 */
+	private String courceName;
+	/** 企業名 */
+	private String companyName;
+	/** 会場名 */
+	private String placeName;
+}
