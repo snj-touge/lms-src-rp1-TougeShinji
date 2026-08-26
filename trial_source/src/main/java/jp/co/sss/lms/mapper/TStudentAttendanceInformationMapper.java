4e2b3915edd294b4a6b1b4ca8a56274a4d271c1f
snj_toge <snj_toge@IBW-A25-019.ibw02.local>
2026-08-26 17:34:40 +0900

    add:峠 Task.57 TStudentAttendanceInformationMapperの新規作成
    勤怠情報確認画面用のマッパークラスを新規作成

---
+++ trial_source/src/main/java/jp/co/sss/lms/mapper/TStudentAttendanceInformationMapper.java
@@ -0,0 +1,31 @@
+package jp.co.sss.lms.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import jp.co.sss.lms.dto.AttendanceInformationDto;
+
+/**
+ * 勤怠情報確認画面用マッパー
+ * 
+ * @author 峠伸治 Task.57
+ */
+@Mapper
+public interface TStudentAttendanceInformationMapper {
+	/**
+	 * @param courseName
+	 * @param placeId
+	 * @param companyName
+	 * @param userName
+	 * @param hddenFlg
+	 * @param deleteFlg
+	 * @return 勤怠情報確認画面用DTOリスト
+	 * @author 峠伸治 - Task.57
+	 */
+	List<AttendanceInformationDto> getAttendanceInformation(@Param("courseName") String courseName,
+			@Param("placeId") Integer placeId, @Param("companyName") String companyName,
+			@Param("userName") String userName, @Param("hiddenFlg") Short hddenFlg,
+			@Param("deleteFlg") Short deleteFlg);
+}
