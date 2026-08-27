9e49579433b7b936da5c016d85554b2846d2daef
snj_toge <snj_toge@IBW-A25-019.ibw02.local>
2026-08-27 10:34:55 +0900

    add:峠 Task.57 MCompanyMapperクラスの新規作成
    MCompanyMapperを新規作成

---
+++ trial_source/src/main/java/jp/co/sss/lms/mapper/MCompanyMapper.java
@@ -0,0 +1,24 @@
+package jp.co.sss.lms.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import jp.co.sss.lms.dto.CompanyDto;
+/**
+ * 企業マスタマッパー
+ * 
+ * @author 峠伸治 - Task.57
+ */
+@Mapper
+public interface MCompanyMapper {
+	
+	/**
+	 * 企業情報を全件取得
+	 * 
+	 * @param deleteFlg
+	 * @return 企業情報DTOリスト
+	 */
+	List<CompanyDto> getCompanyDto(@Param("deleteFlg") Short deleteFlg);
+}
